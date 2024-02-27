61accec85d3b841832f87597a7070f689717cd73
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-27 09:12:54 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My cover letter";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux"; # or something else
+        config = {allowUnfree = true;};
+      };
+
+      watcher = pkgs.writeScriptBin "watch" ''
+        tectonic -X watch -x "build --open"
+      '';
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.bashInteractive];
+        buildInputs = with pkgs; [
+          rubber
+          texlab
+          tectonic
+        ];
+      };
+
+      apps = {
+        watch = {
+          type = "app";
+          program = "${watcher}/bin/watch";
+        };
+      };
+    });
+}
